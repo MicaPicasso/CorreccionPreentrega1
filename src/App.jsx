e1ed0afd22a18e09a9368181b49fa74d4212ffe4
Micaela Picasso <micapicasso130@gmail.com>
2023-07-11 05:40:55 -0300

    Correccion de la primera entrega

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { NavBar} from './header/navBar'
+import { ItemListContainer } from './header/components/itemListContainer'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <ItemListContainer/>
+    </>
+  )
+}
+
+export default App
